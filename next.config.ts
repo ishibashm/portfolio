d13c086a1b881845109655e46353ad8b978528ec
metamo <83742426+ishibashm@users.noreply.github.com>
2026-06-20 18:39:16 +0900

    chore: ignore npm-cache and update config

--- next.config.ts
+++ next.config.ts
@@ -10,14 +10,16 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: process.env.NEXT_PUBLIC_API_URL
-          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
-          : "http://127.0.0.1:8000/api/v1/:path*",
-      },
-    ];
+    return {
+      fallback: [
+        {
+          source: "/api/v1/:path*",
+          destination: process.env.NEXT_PUBLIC_API_URL
+            ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+            : "http://127.0.0.1:8000/api/v1/:path*",
+        },
+      ],
+    };
   },
 };
 
